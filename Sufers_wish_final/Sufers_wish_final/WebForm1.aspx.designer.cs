17b70321c09d73d3a9ed6f8faf2d5e12de3ac5b7
Seungbin <gooahn59@gmail.com>
2015-12-23 13:32:05 +0900

    되돌리기 실행
    옛날 소스 보려고

--- Sufers_wish_final/Sufers_wish_final/WebForm1.aspx.designer.cs
+++ Sufers_wish_final/Sufers_wish_final/WebForm1.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace Sufers_wish_final {
         protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
-        /// Button2 컨트롤입니다.
-        /// </summary>
-        /// <remarks>
-        /// 자동 생성 필드입니다.
-        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
-        
-        /// <summary>
         /// Label1 컨트롤입니다.
         /// </summary>
         /// <remarks>
@@ -47,23 +38,5 @@ namespace Sufers_wish_final {
         /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
-        /// Label2 컨트롤입니다.
-        /// </summary>
-        /// <remarks>
-        /// 자동 생성 필드입니다.
-        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
-        
-        /// <summary>
-        /// Label3 컨트롤입니다.
-        /// </summary>
-        /// <remarks>
-        /// 자동 생성 필드입니다.
-        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
     }
 }
